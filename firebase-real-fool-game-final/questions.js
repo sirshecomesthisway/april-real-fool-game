764bc5c2b9284f3d2d083c413bc324dcbd272e99
Pink Pochemu <107248432+sirshecomesthisway@users.noreply.github.com>
2026-04-25 16:59:33 +0800

    Delete firebase-real-fool-game-final directory

--- firebase-real-fool-game-final/questions.js
+++
@@ -1,4 +0,0 @@
-const questions = [
-  { q: "Walmart uses blockchain to track mangoes", a: "real", artifact: "https://www.ibm.com/blockchain/solutions/food-trust" },
-  { q: "Walmart partnered with SpaceX for delivery", a: "fool", artifact: "https://corporate.walmart.com/newsroom" }
-];
